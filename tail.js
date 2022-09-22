64db08678fa5e0d03e531aad6d8c7549dd5e5ebc
Cassandra Keddis <cassandra@mbp14keddiscas.home>
2022-09-21 21:45:46 -0700

    tail function to grab everything but the first item from an array

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(array, length) {
+  if (array === length) {
+    console.log(`✅ Assertion Passed: You have removed the first item from the array`);
+  } else {
+    console.log(`❌ Assertion Failed: You have not successfully removed the first item from the array`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = arr.slice(1);
+  return newArr;
+};
+
+//Test case 1
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+//Test case 2
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);

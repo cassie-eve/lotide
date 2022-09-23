f270d9c83774db031a6a08014db30c6ab51fb469
Cassandra Keddis <cassandra@mbp14keddiscas.home>
2022-09-22 21:52:54 -0700

    created a function to compare simple arrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: Both arrays are the same!`);
+  } else {
+    console.log(`❌ Assertion Failed: These arrays are not identical`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  if (first.length === second.length) {
+    for (let i = 0; i <= first.length; i++) {
+      if (first[i] === second[i]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays(['close', 1, 2, 3], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays(['butts', 'bears', 'bananas'], ['butts', 'bears', 'bananas']), true); // => should PASS

bc355d7f40d8435f806d621d3443d8386b93b6e4
Cassandra Keddis <cassandra@mbp14keddiscas.home>
2022-09-23 18:31:18 -0700

    lotide function to remove objects from an array

---
+++ without.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} is equal to ${arr2}!`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} is NOT equal to ${arr2}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  if (first.length === second.length) {
+    for (let i = 0; i <= first.length; i++) {
+      if (first[i] === second[i]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let newArr = [];
+  for (const arr of source) {
+    if (!itemsToRemove.includes(arr)) {
+      newArr.push(arr);
+    }
+  }
+  return newArr;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);

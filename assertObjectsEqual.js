4f524eae7165de495f51c499d56419d40af34afd
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-09-28 20:57:01 -0700

    function for object assertations

---
+++ assertObjectsEqual.js
@@ -0,0 +1,29 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } 
+  for (let key of Object.keys(object1)) {
+    if (object1[key] !== object2[key]) {
+      if (Array.isArray(object1[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  } 
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}!`);
+  } else {
+    console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+console.log(assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: "2" }));
+console.log(assertObjectsEqual({ a: "1", b: "2", c: "2" }, { a: "1", b: "2", c: "2" }));

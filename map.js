58497a9edefaf262de62ed2b2b2662cb3026009b
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-09-30 19:38:04 -0700

    created a few tests using assertArraysEqual

--- map.js
+++ map.js
@@ -1,4 +1,26 @@
+const eqArrays = function(first, second) {
+  if (first.length === second.length) {
+    for (let i = 0; i <= first.length; i++) {
+      if (first[i] === second[i]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}!`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
+const numbers = [1, 2, 3, 4, 5];
+const superheroes = ['batman', 'superman', 'spiderman']
 
 const map = function(array, callback) {
   const results = [];
@@ -8,6 +30,7 @@ const map = function(array, callback) {
   return results;
 };
 
-
-const results1 = map(words, word => word[0]);
-console.log(results1);
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(map(superheroes, str => str.slice(0, -3)), ['bat', 
+'super', 'spider']);
+assertArraysEqual(map(numbers, num => num + 1), [2, 3, 4, 5, 6]);

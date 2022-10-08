a6dcf5055e00574a94cd1281e16bc7c8a3b6b5ec
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-07 20:38:10 -0700

    refactor eqArrays and created a separate test file, eslinted

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: Both arrays are the same!`);
-  } else {
-    console.log(`❌ Assertion Failed: These arrays are not identical`);
-  }
-};
-
 const eqArrays = function(first, second) {
   if (first.length === second.length) {
     for (let i = 0; i <= first.length; i++) {
@@ -18,8 +10,4 @@ const eqArrays = function(first, second) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays(['close', 1, 2, 3], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays(['butts', 'bears', 'bananas'], ['butts', 'bears', 'bananas']), true); // => should PASS
-assertEqual(eqArrays([], []), true); // => should PASS
+module.exports = eqArrays;

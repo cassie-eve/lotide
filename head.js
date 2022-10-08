052c6bac2f0a54a15bfbb22b48c3fe2624f76ff5
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-07 20:03:19 -0700

    refactor head and separate test file

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(arr, first) {
-  if (arr === first) {
-    console.log(`✅ Assertion Passed: ${first} is the first item from the array`);
-  } else {
-    console.log(`❌ Assertion Failed: ${first} is not the first item in the array`);
-  }
-};
+// Returns the first item in an array
 
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;

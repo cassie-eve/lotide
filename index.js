e0c8af93465b6ed59108faaf840ad175804a1fa1
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-10 18:08:02 -0700

    introducing object shorthand

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const takeUntil = require('./takeUntil');
 const without   = require('./without');
 
 module.exports = {
-  assertArraysEqual:   assertArraysEqual,
-  assertEqual:   assertEqual,
-  assertObjectsEqual:   assertObjectsEqual,
-  countLetters:   countLetters,
-  countOnly:   countOnly,
-  eqArrays:   eqArrays,
-  findKey:   findKey,
-  findKeyByValue:   findKeyByValue,
-  flatten:   flatten,
-  head:   head,
-  letterPositions:   letterPositions,
-  map:   map,
-  middle:   middle,
-  tail:   tail,
-  takeUntil:   takeUntil,
-  without:   without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };

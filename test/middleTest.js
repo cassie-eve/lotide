fd9089869a8f5864f250a697ccce2d9fab645ec6
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-10 17:25:36 -0700

    refactor with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle(['banana', 'coconut', 'strawberry']), ['coconut']);
-assertArraysEqual(middle([23, 43, 12, 3, 8]), [12]);
-assertArraysEqual(middle([23, 43, 84, 12, 3, 8]), [84, 12]);
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns ['coconut'] for ['banana', 'coconut', 'strawberry']", () => {
+    assert.deepEqual(middle(['banana', 'coconut', 'strawberry']), ['coconut']);
+  });
+  it("returns [12] for [23, 43, 12, 3, 8]", () => {
+    assert.deepEqual(middle([23, 43, 12, 3, 8]), [12]);
+  });
+  it("returns [84, 12] for [23, 43, 84, 12, 3, 8]", () => {
+    assert.deepEqual(middle([23, 43, 84, 12, 3, 8]), [84, 12]);
+  });
+});

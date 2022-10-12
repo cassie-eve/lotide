8f6464881986449b15f4e7c09081304e1a5586fe
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-12 07:59:50 -0400

    refactor, testing, and linting findKey.js

--- findKey.js
+++ findKey.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Takes in an object and callback and returns the first key that meets the criteria specified in callback
 
-const findKey = function (object, callback) {
+const findKey = function(object, callback) {
   for (let key in object) {
     if (callback(object[key])) {
       return key;
@@ -15,15 +9,3 @@ const findKey = function (object, callback) {
 };
 
 module.exports = findKey;
-
-const myObjects = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-assertEqual(findKey(myObjects, x => x.stars === 2), "noma");
-assertEqual(findKey(myObjects, x => x.stars === 1), "Blue Hill");
---
+++ test/findKeyTest.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe("#findKey", () => {
+  const myObjects = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  };
+  it("should return 'noma'", () => {
+    assert.strictEqual(findKey(myObjects, x => x.stars === 2), "noma");
+  });
+  it("should return 'Blue Hill'", () => {
+    assert.strictEqual(findKey(myObjects, x => x.stars === 1), "Blue Hill"); 
+  });
+});

33383db2fa9fb919e15bbaa705dd6817ffbb0dde
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-12 04:04:09 -0700

    imported functions rather than copy paste

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,4 @@
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } 
-  for (let key of Object.keys(object1)) {
-    if (object1[key] !== object2[key]) {
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    }
-  } 
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,11 @@
 // Function implementation
+const eqArrays = require('./eqArrays');
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(first, second) {
-  if (first.length === second.length) {
-    for (let i = 0; i <= first.length; i++) {
-      if (first[i] === second[i]) {
-        return true;
-      } else {
-        return false;
-      }
-    }
   }
 };
 

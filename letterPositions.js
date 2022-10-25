a127d368343ce2a50c5950670991846730145cba
Cassandra Keddis <cassandra.keddis@gmail.com>
2022-10-24 19:45:49 -0700

    fix bug in letter positions

--- letterPositions.js
+++ letterPositions.js
@@ -3,7 +3,7 @@
 const letterPositions = function(sentence) {
   const results = {};
   sentence = sentence.replaceAll(' ', '');
-  for (let i = 0; i <= sentence.length; i++) {
+  for (let i = 0; i < sentence.length; i++) {
     if (results[sentence[i]]) {
       results[sentence[i]].push(i);
     } else {
